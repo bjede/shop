59961704068084c427c328378b5343a01bee05a1
Aldin <aldin.bijedic@gmx.at>
2022-04-15 17:06:28 +0200

    Create format.class.js

---
+++ models/format.class.js
@@ -0,0 +1,14 @@
+class Format {
+
+    static shortenText(text, limit) {
+        let description = text;
+        let stringLength = description.length
+        let stringDifference = stringLength - limit;
+        let finalString = description.substring(0, stringLength - stringDifference);
+        return (limit < stringLength) ? finalString + '...' : description;
+    }
+
+    static currency(obj, currency) {
+        return obj.toFixed(2).replace('.', ',') + ' ' + currency;
+    }
+}
